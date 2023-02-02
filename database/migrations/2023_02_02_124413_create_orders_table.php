d500244793aaf036d56c18fc02186b8995272257
sammashok <sammashtiunz@gmail.com>
2023-02-02 20:53:57 +0100

    Make `Order` model and migration

---
+++ database/migrations/2023_02_02_124413_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('amount')->default('1000');
+            $table->enum('delivery_type', ['pickup','delivery','pos'])->default(0);
+            $table->string('delivery_address')->nullable();
+            $table->string('notes')->nullable();
+            $table->timestamp('delivered_at')->nullable();
+            $table->authors();
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
