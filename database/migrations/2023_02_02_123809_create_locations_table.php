ac32fe3ec05a1fa2feee524338c13dbc890d1341
newtonjob <jobnewton3@gmail.com>
2023-02-02 13:47:16 +0100

    Create Location & Category Models with Categories Migration

---
+++ database/migrations/2023_02_02_123809_create_locations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->unique();
+            $table->string("slug")->unique();
+            $table->integer("address")->nullable();
+            $table->timestamp("featured_at")->nullable();
+            $table->foreignId("created_by")->nullable()->constrained("users");
+            $table->foreignId("updated_by")->nullable()->constrained("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+};
