7c4eb1bd3f15159326dd57c2e76d9d3a3858419d
newtonjob <jobnewton3@gmail.com>
2023-02-02 13:37:04 +0100

    Create Categories Table

---
+++ database/migrations/2023_02_02_121627_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->unique();
+            $table->string("image")->nullable();
+            $table->string("slug")->unique();
+            $table->integer("relevance")->default(0);
+            $table->timestamp("featured_at")->nullable();
+            $table->foreignId("created_by")->nullable()->constrained("users");
+            $table->foreignId("updated_by")->nullable()->constrained("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
