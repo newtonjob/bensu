ab62dd77e315e1a71b581bf0b7b490c03452ea65
newtonjob <jobnewton3@gmail.com>
2023-02-02 14:10:41 +0100

    Update Users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,28 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('phone', 45)->nullable()->unique();
+            $table->string('first_name', 45);
+            $table->string('last_name', 45)->nullable();
+            $table->enum('gender', ['M', 'F'])->default('M')->index();
+            $table->string('photo')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('type', ['admin', 'customer'])->default('customer');
+            $table->foreignId('role_id')->nullable()->constrained();
+            $table->boolean('notifiable')->default(false);
+            $table->integer('login_count')->default(0);
+            $table->timestamp('last_login')->nullable();
+            $table->timestamp('banned_until')->nullable();
+            $table->json('meta')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->authors();
+            $table->softDeletes();
+
+            $table->fullText(['last_name', 'first_name', 'phone', 'email']);
         });
     }
 
