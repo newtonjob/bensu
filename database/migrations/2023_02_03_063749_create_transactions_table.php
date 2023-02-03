6e81fb3bd2ae41e1dc6f4ff8b03d204efddef8b3
Ogar <ogarjobbassey@gmail.com>
2023-02-03 09:40:04 +0100

    Create `Transaction` model with migration

---
+++ database/migrations/2023_02_03_063749_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->string('reference')->unique();
+            $table->float('amount');
+            $table->enum('channel', ['M', 'P', 'C', 'B'])->default('P');
+            $table->timestamp('paid_at')->nullable();
+            $table->authors();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
