416e1ff7bae6488b6473ab09c4d3cacf13fc55a4
newtonjob <jobnewton3@gmail.com>
2023-02-03 21:14:21 +0100

    Define `subCategory` relationship and `isFeatured` model  helper

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * Determine if the category is featured.
+     */
+    public function isFeatured()
+    {
+        return (bool) $this->featured_at;
+    }
+
+    public function subCategories()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
 }
