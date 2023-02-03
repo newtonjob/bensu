a95c0a5d84690c533c7f9112336f5e338fc3a933
Ogar <ogarjobbassey@gmail.com>
2023-02-03 09:59:42 +0100

    Update `settings` migration

--- database/migrations/2023_02_03_081209_create_settings_table.php
+++ database/migrations/2023_02_03_081209_create_settings_table.php
@@ -18,14 +18,14 @@ return new class extends Migration
             $table->string('logo');
             $table->string('title');
             $table->string('description')->nullable();
-            $table->json('banner')->nullable();
+            $table->json('banners')->nullable();
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->json('social_links')->nullable();
             $table->json('about')->nullable();
             $table->string('footer_quote')->nullable();
-            $table->integer('usd_exchange_rates')->default(400);
+            $table->integer('usd_exchange_rate')->default(500);
             $table->json('meta')->nullable();
             $table->authors();
             $table->timestamps();
