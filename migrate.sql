542a7393144c56f2413d534bf9ccfe0ee03f3c9a
newtonjob <jobnewton3@gmail.com>
2023-02-03 17:21:50 +0100

    Implement protected guarded property for `Image` model

--- migrate.sql
+++ migrate.sql
@@ -10,7 +10,6 @@ SELECT `id`, `permission`, `description`, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP F
 INSERT INTO `users`(`id`, `email`, `email_verified_at`, `password`, `phone`, `first_name`, `last_name`, `gender`, `photo`, `address`, `type`, `role_id`, `notifiable`, `login_count`, `last_login`, `banned_until`, `meta`, `remember_token`, `created_at`, `updated_at`, `created_by`, `updated_by`, `deleted_at`)
 SELECT `user_id`, `email`, IF(_email_verified, created, NULL), `password`, `phone`, `fname`, `lname`, `gender`, `photo`, `address`, `user_type`, NULL, 0, login_count, last_login, IF(status, NULL, '2123-01-01 00:00:00'), `meta`, NULL, `created`, updated, `created_by`, `updated_by`, IF(is_deleted, updated, NULL) FROM `bensu-ci`.`user`;
 
-
 /**
   MIGRATE brands
  */
@@ -23,22 +22,17 @@ SELECT `brand_id`, `brand_name`, `brand_img`, `slug`, `created_by`, `updated_by`
 INSERT INTO `categories`(`id`, `name`, `image`, `slug`, `relevance`, `featured_at`, `created_by`, `updated_by`, `created_at`, `updated_at`)
 SELECT `cat_id`, `cat_name`, `cat_img`, `slug`, IFNULL(`_order`, 0), IF(`featured`, `updated`, NULL), `created_by`, `updated_by`, `created`, `updated` FROM `bensu-ci`.`category`;
 
-
-
-
 /**
   MIGRATE colors
  */
 INSERT INTO `colors`(`id`, `name`, `slug`, `created_by`, `updated_by`, `created_at`, `updated_at`)
 SELECT `color_id`, `color_name`, `slug`,  `created_by`, `updated_by`, `created`, `updated`  FROM `bensu-ci`.`color`;
 
-
 /**
   MIGRATE locations
  */
 INSERT INTO `locations`(`id`, `name`, `slug`, `address`, `featured_at`, `created_by`, `updated_by`, `created_at`, `updated_at`)
 SELECT `location_id`, `location_name`, `slug`, `location_address`, IF(featured, updated, NULL), `created_by`, `updated_by`, `created`, `updated` FROM `bensu-ci`.`location`;
-
 
 /**
   MIGRATE orders
@@ -52,13 +46,11 @@ SELECT `order_id`, `user_id`, `delivery_type`+1, `delivery_address`, IF(`deliver
 INSERT INTO `sub_categories`(`id`, `category_id`, `name`, `slug`, `created_by`, `updated_by`, `created_at`, `updated_at`)
 SELECT `product_id`, `cat_id`, `product_name`, `slug`, `created_by`, `updated_by`, `created`, `updated` FROM `bensu-ci`.`product`;
 
-
 /**
   MIGRATE products
  */
 INSERT INTO `products`(`id`, `sub_category_id`, `brand_id`, `color_id`, `name`, `description`, `tags`, `cost_price`, `price`, `currency`, `discount`, `model_no`, `serial_no`, `featured_at`, `slug`, `subscribers`, `created_by`, `updated_by`, `created_at`, `updated_at`)
 SELECT `item_id`, `product_id`, `brand_id`, `color_id`, `item_name`, `item_desc`, `tags`, `cost_price`, `price`, `price_currency`, `discount`, `model_number`, `serial_number`, IF(`featured`, `updated`, NULL), `slug`, `get_stock_alert`, `created_by`, `updated_by`, `created`, `updated` FROM `bensu-ci`.`product_item`;
-
 
 /**
   MIGRATE order_product
@@ -66,13 +58,11 @@ SELECT `item_id`, `product_id`, `brand_id`, `color_id`, `item_name`, `item_desc`
 INSERT INTO `order_product`(`id`, `order_id`, `product_id`, `price`, `quantity`, `location_id`, `meta`, `updated_by`, `created_at`, `updated_at`)
 SELECT `id`, `order_id`, `item_id`, `price`, `quantity`, `location_id`, `meta`, `updated_by`, `created`, `updated` FROM `bensu-ci`.`order_item`;
 
-
 /**
   MIGRATE order_product
  */
 INSERT INTO `wishes`(`id`, `user_id`, `product_id`, `created_at`, `updated_at`)
 SELECT `id`, `user_id`, `item_id`, `created`, `created` FROM `bensu-ci`.`user_wishlist`;
-
 
 /**
   MIGRATE wishes
@@ -80,13 +70,11 @@ SELECT `id`, `user_id`, `item_id`, `created`, `created` FROM `bensu-ci`.`user_wi
 INSERT INTO `wishes`(`id`, `user_id`, `product_id`, `created_at`, `updated_at`)
 SELECT `id`, `user_id`, `item_id`, `created`, `created` FROM `bensu-ci`.`user_wishlist`;
 
-
 /**
   MIGRATE stocks
  */
 INSERT INTO `stocks`(`id`, `product_id`, `location_id`, `quantity`, `created_by`, `updated_by`, `created_at`, `updated_at`)
 SELECT `id`, `item_id`, `location_id`, `quantity`, `created_by`, `updated_by`, `created`, `updated`  FROM `bensu-ci`.`product_item_quantity`;
-
 
 /**
   MIGRATE reviews
@@ -105,7 +93,6 @@ SELECT `id`, `order_id`, `reference`, `amount`, IF(vendor = 'M', 1, vendor - 1),
  */
 INSERT INTO `settings`(`id`, `logo`, `title`, `description`, `banners`, `email`, `phone`, `address`, `social_links`, `about`, `footer_quote`, `usd_exchange_rate`, `meta`, `updated_at`, `updated_by`)
 SELECT `id`, `site_logo`, `site_title`, `site_desc`, `banner`, `site_email`, `site_phone`, `site_address`, `social_media_links`, `about`, `footer_quote`, `exchange_rate_usd`, `meta`, `updated`, `updated_by` FROM `bensu-ci`.`site_info`;
-
 
 /**
   MIGRATE transfers
