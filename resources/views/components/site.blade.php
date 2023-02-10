62f2adc5b3359d56484d8fd657da6429732b6236
sammashok <sammashtiunz@gmail.com>
2023-02-10 22:30:14 +0100

    Refactor `head` components in `site.blade.php`

--- resources/views/components/site.blade.php
+++ resources/views/components/site.blade.php
@@ -4,31 +4,37 @@
     <meta charset=utf-8>
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1">
-    <meta name=keywords
-          content="auto parts, baby store, ecommerce, electronics, fashion, food, marketplace, modern, multi vendor, multipurpose, organic, responsive, shop, shopping, store">
-    <meta name=description content="Zeomart - Multi-Vendor & Marketplace HTML Template">
+    <meta name="description" content="{{ $description ?? site('description') }}">
+    <meta name="keywords" content="{{ $keywords ?? '' }}">
+    <meta name="author" content="Newton Job - 07011227815">
     <meta name=CreativeLayers content=ATFN>
-    <link rel=stylesheet href="{{asset('css/bootstrap.min.css')}}">
-    <link rel=stylesheet href="{{asset('css/ace-responsive-menu.css')}}">
-    <link rel=stylesheet href="{{asset('css/menu.css')}}">
-    <link rel=stylesheet href="{{asset('css/fontawesome.css')}}">
-    <link rel=stylesheet href="{{asset('css/flaticon.css')}}">
-    <link rel=stylesheet href="{{asset('css/bootstrap-select.min.css')}}">
-    <link rel=stylesheet href="{{asset('css/animate.css')}}">
-    <link rel=stylesheet href="{{asset('css/slider.css')}}">
-    <link rel=stylesheet href="{{asset('css/style.css')}}">
+    <meta property="og:site_name" content="{{ config('app.name') }}"/>
+    <meta property="og:title" content="{{ $title ?? '' }}" />
+    <meta property="og:description" content="{{ $description ?? '' }}" />
+    <meta property="og:image" content="{{ $image ?? '' }}" />
+    <link rel=stylesheet href="{{ asset('css/bootstrap.min.css')}}">
+    <link rel=stylesheet href="{{ asset('css/ace-responsive-menu.css')}}">
+    <link rel=stylesheet href="{{ asset('css/menu.css')}}">
+    <link rel=stylesheet href="{{ asset('css/fontawesome.css')}}">
+    <link rel=stylesheet href="{{ asset('css/flaticon.css')}}">
+    <link rel=stylesheet href="{{ asset('css/bootstrap-select.min.css')}}">
+    <link rel=stylesheet href="{{ asset('css/animate.css')}}">
+    <link rel=stylesheet href="{{ asset('css/slider.css')}}">
+    <link rel=stylesheet href="{{ asset('css/style.css')}}">
     <link rel=preconnect href="https://fonts.googleapis.com">
     <link rel=preconnect href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500&family=Poppins:wght@700&display=swap"
           rel=stylesheet>
     <link rel=stylesheet href="{{asset('css/responsive.css')}}">
-    <title>Zeomart - Multi-Vendor & Marketplace HTML Template</title>
-    <link href="{{asset('images/favicon.ico')}}'" sizes=128x128 rel="shortcut icon" type=image/x-icon>
-    <link href="{{asset('images/favicon.ico')}}'" sizes=128x128 rel="shortcut icon">
-    <link href="{{asset('images/apple-touch-icon-60x60.png')}}" sizes=60x60 rel=apple-touch-icon>
-    <link href="{{asset('images/apple-touch-icon-72x72.png')}}" sizes=72x72 rel=apple-touch-icon>
-    <link href="{{asset('images/apple-touch-icon-114x114.png')}}" sizes=114x114 rel=apple-touch-icon>
-    <link href="{{asset('images/apple-touch-icon-180x180.png')}}" sizes=180x180 rel=apple-touch-icon>
+    <title>{{ $title ?? config('app.name') }}</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/img/logo/favicon.png') }}">
+    <link rel="shortcut icon" type="image/png" sizes="16x16" href="{{ asset('/img/logo/favicon.png') }}">
+
+    <link href="{{ asset('images/apple-touch-icon-60x60.png')}}" sizes=60x60 rel=apple-touch-icon>
+    <link href="{{ asset('images/apple-touch-icon-72x72.png')}}" sizes=72x72 rel=apple-touch-icon>
+    <link href="{{ asset('images/apple-touch-icon-114x114.png')}}" sizes=114x114 rel=apple-touch-icon>
+    <link href="{{ asset('images/apple-touch-icon-180x180.png')}}" sizes=180x180 rel=apple-touch-icon>
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
