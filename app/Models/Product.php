03e7970e27603711d46b257d2d43d771b53ec787
Ogar <ogarjobbassey@gmail.com>
2023-02-13 14:44:14 +0100

    Add scopes for `featured` and `discounted` in `Product` model.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,26 +2,49 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
 
-    public function isFeatured()
+    /**
+     * Scope the query to only include featured products.
+     */
+    public function scopeFeatured(Builder $builder)
+    {
+        $builder->whereNotNull('featured_at');
+    }
+    /**
+     * Scope the query to only include discounted products.
+     */
+    public function scopeDiscounted(Builder $builder)
+    {
+        $builder->where('discount', '>', 0);
+    }
+    public function isFeatured(): bool
     {
         return (bool) $this->featured_at;
     }
 
-    public function images()
+    public function images(): HasMany
     {
         return $this->hasMany(Image::class);
     }
 
-    public function brand()
+    public function brand(): BelongsTo
     {
         return $this->belongsTo(Brand::class);
+    }
+
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class, OrderProduct::class)->withTimestamps();
     }
 }
