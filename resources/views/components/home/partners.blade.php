f55a8de6757414db0104fffe7e659188045f02d7
sammashok <sammashtiunz@gmail.com>
2023-02-13 16:08:59 +0100

    Update `partners.blade.php` component.

--- resources/views/components/home/partners.blade.php
+++ resources/views/components/home/partners.blade.php
@@ -2,36 +2,14 @@
     <div class=container>
         <div class=row>
             <div class=col-lg-12>
-                <div class="text-center mb35">
-                    <h5 class=fz16>Trusted by the world’s best</h5>
-                </div>
-            </div>
-            <div class=col-lg-12>
                 <div class="shop_item_6grid_slider slider_dib_sm dots_none nav_none owl-carousel owl-theme">
-                    <div class=item>
-                        <div class="partner_item wow fadeInUp" data-wow-duration=0.3s><img
-                                src="{{asset('images/partners/1.png')}}" alt=1.png></div>
-                    </div>
-                    <div class=item>
-                        <div class="partner_item wow fadeInUp" data-wow-duration=0.5s><img
-                                src="{{asset('images/partners/2.png')}}" alt=2.png></div>
-                    </div>
-                    <div class=item>
-                        <div class="partner_item wow fadeInUp" data-wow-duration=0.7s><img
-                                src="{{asset('images/partners/3.png')}}" alt=3.png></div>
-                    </div>
-                    <div class=item>
-                        <div class="partner_item wow fadeInUp" data-wow-duration=0.9s><img
-                                src="{{asset('images/partners/4.png')}}" alt=4.png></div>
-                    </div>
-                    <div class=item>
-                        <div class="partner_item wow fadeInUp" data-wow-duration=1.1s><img
-                                src="{{asset('images/partners/5.png')}}" alt=5.png></div>
-                    </div>
-                    <div class=item>
-                        <div class="partner_item wow fadeInUp" data-wow-duration=1.3s><img
-                                src="{{asset('images/partners/6.png')}}" alt=6.png></div>
-                    </div>
+                    @foreach(app('brands')->take(6) as $brand)
+                        <div class=item>
+                            <div class="partner_item wow fadeInUp" data-wow-duration="{{ 0.3 * $loop->iteration }}">
+                                <img src="{{ cloudinary_url($brand->image, 100) }}" alt="{{$brand->name}} Image">
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
