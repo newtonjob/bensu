32dbf8e1ddd632d50affb327159da8ec9624c72f
sammashok <sammashtiunz@gmail.com>
2023-02-14 13:42:49 +0100

    Refactor and update footer contents.

--- resources/views/components/site/footer.blade.php
+++ resources/views/components/site/footer.blade.php
@@ -5,7 +5,7 @@
                 <div class="mailchimp_widget mb30-md text-center">
                     <div class="icon float-start"><span class=flaticon-email-1></span></div>
                     <div class=details>
-                        <h3 class=title>Subscribe and get 20% discount.</h3>
+                        <h3 class=title>Join Our Newsletter Now</h3>
                     </div>
                 </div>
                 <div class=footer_social_widget>
@@ -23,54 +23,55 @@
         <div class="row mt60">
             <div class="col-sm-6 col-md-5 col-lg-3 col-xl-3">
                 <div class=footer_contact_widget>
-                    <h4>Contact Us</h4>
+                    <a href="#"><img class="mb-4" style="height: 34px; width: 162px" src="{{ asset('img/logo/logo.png') }}" alt="logo"></a>
+                    <h4>Address: <span class="h6">{{ site('address') }}</span></h4>
                     <div class="footer_contact_iconbox d-flex mb-4">
                         <div class=icon><span class=flaticon-phone-call></span></div>
                         <div class="details ms-4">
-                            <h5 class=title>Monday-Friday: 08am-9pm</h5>
-                            <a href="index.html#">+(1) 123 456 7890</a></div>
+                            <h5 class=title>Mobile</h5>
+                            <a href="#">{{ substr(site('phone'), 0, 31) }}</a>
+                            <p><a href="#">{{ substr(site('phone'), -14) }}</a></p>
+                        </div>
                     </div>
                     <div class="footer_contact_iconbox d-flex">
                         <div class=icon><span class=flaticon-email></span></div>
                         <div class="details ms-4">
-                            <h5 class=title>Need help with your order?</h5>
-                            <a href="index.html#">support@zeomart.com</a></div>
+                            <h5 class=title>Support</h5>
+                            <a target="_blank" href="mailto:{{ site('email') }}">{{ site('email') }}</a></div>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-3 col-lg-2 col-xl-2">
                 <div class=footer_qlink_widget>
-                    <h4>About Zenmart</h4>
+                    <h4>Top Categories</h4>
+                        <ul class=list-unstyled>
+                            @foreach (app('categories')->take(5) as $category)
+                                <li class="">
+                                    <a href="{{ url("shop?category={$category->slug}") }}">{{ $category->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 col-lg-2 col-xl-2">
+                <div class=footer_qlink_widget>
+                    <h4>Information</h4>
                     <ul class=list-unstyled>
-                        <li><a href="index.html#">Track Your Order</a></li>
-                        <li><a href="index.html#">Product Guides</a></li>
-                        <li><a href="index.html#">Wishlists</a></li>
-                        <li><a href="index.html#">Privacy Policy</a></li>
-                        <li><a href="index.html#">Store Locator</a></li>
+                        <li><a href="{{ url('home/account') }}">My Account</a></li>
+                        <li><a href="{{ url('account#wishlist') }}">My Items</a></li>
+                        <li><a href="#">My Cart</a></li>
+                        <li><a href="{{ url('shop') }}">Shop</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-sm-6 col-md-3 col-lg-2 col-xl-2">
                 <div class=footer_qlink_widget>
-                    <h4>Customer Support</h4>
+                    <h4>Our Services</h4>
                     <ul class=list-unstyled>
-                        <li><a href="index.html#">Contact Us</a></li>
-                        <li><a href="index.html#">Help Centre</a></li>
-                        <li><a href="index.html#">Returns & Exchanges</a></li>
-                        <li><a href="index.html#">Best Buy Financing</a></li>
-                        <li><a href="index.html#">Best Buy Gift Card</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-sm-6 col-md-3 col-lg-2 col-xl-2">
-                <div class=footer_qlink_widget>
-                    <h4>Services</h4>
-                    <ul class=list-unstyled>
-                        <li><a href="index.html#">Geek Squad</a></li>
-                        <li><a href="index.html#">In-Home Advisor</a></li>
-                        <li><a href="index.html#">Trade-In Program</a></li>
-                        <li><a href="index.html#">Electronics Recycling</a></li>
-                        <li><a href="index.html#">Best Buy Health</a></li>
+                        <li><a href="{{ url('home/about-us') }}">About us</a></li>
+                        <li><a href="{{ url('home/contact-us') }}">Contact Us</a></li>
+                        <li><a href="#">Refund</a></li>
+                        <li><a href="{{ url('home/privacy-policy') }}">Privacy Policy</a></li>
                     </ul>
                 </div>
             </div>
@@ -79,23 +80,18 @@
                     <h4 class=title>Follow us</h4>
                     <div class="social_icon_list mt30">
                         <ul class=mb20>
-                            <li class=list-inline-item><a href="index.html#"><i class="fab fa-facebook"></i></a>
+                            <li class=list-inline-item>
+                                <a href="{{ site('social_links')->facebook }}"><i class="fab fa-facebook"></i></a>
                             </li>
-                            <li class=list-inline-item><a href="index.html#"><i class="fab fa-twitter"></i></a>
+                            <li class=list-inline-item><a href="{{ site('social_links')->twitter }}">
+                                    <i class="fab fa-twitter"></i></a>
                             </li>
-                            <li class=list-inline-item><a href="index.html#"><i
-                                        class="fab fa-instagram"></i></a></li>
-                            <li class=list-inline-item><a href="index.html#"><i class="fab fa-linkedin-in"></i></a>
+                            <li class=list-inline-item><a href="{{ site('social_links')->instagram }}">
+                                    <i class="fab fa-instagram"></i></a>
                             </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="footer_mobile_app_widget mb25">
-                    <h4 class="title mb10">Mobile Apps</h4>
-                    <div class=mobile_app_list>
-                        <ul class=mb0>
-                            <li><a href="index.html#"><span class=flaticon-apple></span>iOS App</a></li>
-                            <li><a href="index.html#"><span class=flaticon-android></span>Android App</a></li>
+                            <li class=list-inline-item><a href="{{ site('social_links')->linkedin }}">
+                                    <i class="fab fa-linkedin-in"></i></a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -103,18 +99,24 @@
                     <h4 class="title mb20">We accept</h4>
                     <div class=acceped_card_list>
                         <ul class="d-flex mb-0">
-                            <li class=me-2><a href="index.html#"><img src="{{asset('images/resource/visa-card.png')}}"
-                                                                      alt=visa-card></a></li>
-                            <li class=me-2><a href="index.html#"><img src="{{asset('images/resource/master-card.png')}}"
-                                                                      alt=master-card></a></li>
-                            <li class=me-2><a href="index.html#"><img src="{{asset('images/resource/apple-pay.png')}}"
-                                                                      alt=apple-pay></a></li>
-                            <li class=me-2><a href="index.html#"><img src="{{asset('images/resource/discover-card.png')}}"
-                                                                      alt=discover-card></a></li>
-                            <li class=me-2><a href="index.html#"><img src="{{asset('images/resource/paypal.png')}}"
-                                                                      alt=paypal></a></li>
-                            <li><a href="index.html#"><img src="{{asset('images/resource/amex-card.png')}}"
-                                                           alt=amex-card></a></li>
+                            <li class=me-2>
+                                <a href="#"><img src="{{asset('images/resource/visa-card.png')}}" alt=visa-card></a>
+                            </li>
+                            <li class=me-2>
+                                <a href="#"><img src="{{asset('images/resource/master-card.png')}}" alt=master-card></a>
+                            </li>
+                            <li class=me-2>
+                                <a href="#"><img src="{{asset('images/resource/apple-pay.png')}}" alt=apple-pay></a>
+                            </li>
+                            <li class=me-2>
+                                <a href="#"><img src="{{asset('images/resource/discover-card.png')}}" alt=discover-card></a>
+                            </li>
+                            <li class=me-2>
+                                <a href="#"><img src="{{asset('images/resource/paypal.png')}}" alt=paypal></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{asset('images/resource/amex-card.png')}}" alt=amex-card></a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -125,31 +127,11 @@
         <div class=row>
             <div class=col-lg-6>
                 <div class="copyright-widget text-center text-lg-start d-block d-lg-flex mb15-md">
-                    <p class=me-4>© 2022 Zeomart. All Rights Reserved</p>
-                    <p><a href="index.html#">Privacy</a>·<a href="index.html#">Terms</a>·<a href="index.html#">Sitemap</a>
-                    </p>
-                </div>
-            </div>
-            <div class=col-lg-6>
-                <div class="footer_bottom_right_widgets text-center text-lg-end">
-                    <ul class=mb0>
-                        <li class="list-inline-item mb20-340">
-                            <select class="selectpicker show-tick">
-                                <option>Currency : USD</option>
-                                <option>Euro</option>
-                                <option>Pound</option>
-                            </select>
-                        </li>
-                        <li class=list-inline-item>
-                            <select class="selectpicker show-tick">
-                                <option>Language: English</option>
-                                <option>Frenc</option>
-                                <option>Italian</option>
-                                <option>Spanish</option>
-                                <option>Turkey</option>
-                            </select>
-                        </li>
-                    </ul>
+                    <p class=me-4>©
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                       {{ site('title') }}. All Rights Reserved.</p>
                 </div>
             </div>
         </div>
