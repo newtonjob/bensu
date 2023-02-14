34722c084500699223dec26411266b4272589e26
Ogar <ogarjobbassey@gmail.com>
2023-02-14 17:49:03 +0100

    Make `home.features` a more reusable component

--- resources/views/components/home/features.blade.php
+++ resources/views/components/home/features.blade.php
@@ -1,42 +1,10 @@
-<section class="features pt30 pb20">
-    <div class="container bb1">
-        <div class="row ovh">
-            <div class="col-sm-6 col-xl-3">
-                <div class="icon_boxes d-flex wow fadeInUp" data-wow-duration=1.0s>
-                    <div class=icon><span class=flaticon-fast-delivery></span></div>
-                    <div class=details>
-                        <h5 class=title>Free Shipping</h5>
-                        <p class=para>Free delivery on special locations</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="icon_boxes d-flex wow fadeInUp" data-wow-duration=1.2s>
-                    <div class=icon><span class=flaticon-shield></span></div>
-                    <div class=details>
-                        <h5 class=title>100% Money Guarantee</h5>
-                        <p class=para>You have 30 days to return</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="icon_boxes d-flex wow fadeInUp" data-wow-duration=1.4s>
-                    <div class=icon><span class=flaticon-headphones></span></div>
-                    <div class=details>
-                        <h5 class=title>Online Support</h5>
-                        <p class=para>Contact Us 24 hours everyday</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="icon_boxes d-flex wow fadeInUp" data-wow-duration=1.6s>
-                    <div class=icon><span class=flaticon-credit-card></span></div>
-                    <div class=details>
-                        <h5 class=title>Flexible & Secure Payment</h5>
-                        <p class=para>We ensure Secure payments with multiple credit cards</p>
-                    </div>
-                </div>
-            </div>
+@props(['title', 'paragraph', 'icon', 'transition'])
+<div class="col-sm-6 col-xl-3">
+    <div class="icon_boxes d-flex wow fadeInUp" data-wow-duration={{ $transition }}>
+        <div class=icon><span class={{ "flaticon-{$icon}" }}></span></div>
+        <div class=details>
+            <h5 class=title>{{ $title }}</h5>
+            <p class=para>{{ $paragraph }}</p>
         </div>
     </div>
-</section>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,15 @@
 <x-site>
     <x-home.hero />
-    <x-home.features />
+    <section class="features pt30 pb20">
+        <div class="container bb1">
+            <div class="row ovh">
+                <x-home.features title="Free Shipping" paragraph="Free delivery on special locations" icon="fast-delivery" transition="1.0s"/>
+                <x-home.features title="100% Money Guarantee" paragraph="You have 30 days to return" icon="shield" transition="1.2s" />
+                <x-home.features title="Online Support" paragraph="Contact Us 24 hours everyday" icon="headphones" transition="1.4s" />
+                <x-home.features title="Flexible & Secure Payment" paragraph="We ensure Secure payments with multiple credit cards" icon="credit-card" transition="1.6s" />
+            </div>
+        </div>
+    </section>
     <x-home.deals :discountedProducts="$discountedProducts"/>
     <x-home.categories />
     <x-home.featured-products :bestSellers="$bestSellers" :newProducts="$newProducts"/>
