4d28c9e3439b92ebf4fda7b335073ba76f4d79c9
sammashok <sammashtiunz@gmail.com>
2023-02-17 13:38:03 +0100

    Create shop components to clean up the `shop.blade.php` view

---
+++ resources/views/components/shop/shop-brands.blade.php
@@ -0,0 +1,19 @@
+<div class="row mt30">
+    <div class="col-lg-12">
+        <div class="main-title">
+            <h2 class="title">Shop by brand</h2>
+        </div>
+        <div class="shop_by_brand">
+            <ul class="mb0">
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Apple</a></li>
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Dell</a></li>
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Hp</a></li>
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Lenovo</a></li>
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Samsung</a></li>
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Asus</a></li>
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Acer</a></li>
+            <li class="list-inline-item mb-2 me-3"><a href="page-shop-list-v7.html#">Sony</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
