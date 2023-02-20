9a4bf85f2ac044bc7f2d672d6e8649485ab3f4b8
sammashok <sammashtiunz@gmail.com>
2023-02-20 12:55:02 +0100

    Fix Indentations

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -10,7 +10,6 @@ class ShopController extends Controller
     public function __invoke()
     {
         $products = Product::filter(request(['q', 'category', 'sub-category']))->paginate(16);
-        
 
         return view('shop', compact('products'));
     }
