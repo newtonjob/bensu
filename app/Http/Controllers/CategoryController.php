6b41df4416ef34d3a101076a39a855c595239417
Ogar <ogarjobbassey@gmail.com>
2023-03-13 16:14:11 +0100

    Declutter `CategoryController::index()`

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,10 +12,8 @@ class CategoryController extends Controller
     public function index()
     {
         $categories     = Category::withCount('products')->get();
-        $subCategories  = SubCategory::all();
-        $products = Product::with('images')->withSum('stocks', 'quantity')->filter()->paginate(40);
 
-        return view('categories.index', compact('categories', 'subCategories'));
+        return view('categories.index', compact('categories'));
     }
 
     public function create()
