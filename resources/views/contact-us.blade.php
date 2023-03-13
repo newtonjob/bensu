9c7fccaa95116a4d79880f28b331c70ad2687ae8
sammashok <sammashtiunz@gmail.com>
2023-03-13 08:05:32 +0100

    Update `contact-us.blade.php`

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -1,5 +1,5 @@
 <x-site>
-    <x-site.breadcrumbs title="Contact-us"/>
+    <x-site.breadcrumbs title="Contact us"/>
     <!-- Our Contact -->
     <section class="our-contact pt0 pb0">
         <div class="container">
@@ -57,12 +57,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row ovh pt30 bdrt1">
-                <x-home.feature title="Free Shipping" paragraph="Free delivery on special locations" icon="fast-delivery" transition="1.0s"/>
-                <x-home.feature title="100% Money Guarantee" paragraph="You have 30 days to return" icon="shield" transition="1.2s" />
-                <x-home.feature title="Online Support" paragraph="Contact Us 24 hours everyday" icon="headphones" transition="1.4s" />
-                <x-home.feature title="Flexible & Secure Payment" paragraph="We ensure Secure payments with multiple credit cards" icon="credit-card" transition="1.6s" />
             </div>
         </div>
     </section>
